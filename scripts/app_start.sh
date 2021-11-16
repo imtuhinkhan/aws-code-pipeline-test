8f3989f4e968e145a2015d6cedcad0bbf657dc70
Tuhin <uiu.tuhin@gmail.com>
2021-11-16 14:02:37 +0600

    change directory

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd /home/ec2-user/server/src
+cd /var/www
 npm start
 pm2 start npm --name "react-app" -- start
 pm2 startup
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/ubuntu/server
+cd /var/www
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt install nodejs npm
