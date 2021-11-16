021b7e5d1f12a9da9848680609c9f0ebc77dc3a4
Tuhin <uiu.tuhin@gmail.com>
2021-11-16 14:12:15 +0600

    change directory

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /var/www
-curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt install nodejs npm
+curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt install -y nodejs npm
