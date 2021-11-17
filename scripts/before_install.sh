ad74209bada5359e5ef306d44f2344fb617608dc
Tuhin <uiu.tuhin@gmail.com>
2021-11-17 10:55:01 +0600

    revert script

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 cd /var/www
-if which node > /dev/null
-    then
-        echo "node is installed, skipping..."
-    else
-        curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-        sudo apt-get install nodejs
-fi
+curl -fsSL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
