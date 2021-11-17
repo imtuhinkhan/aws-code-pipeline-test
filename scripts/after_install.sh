1ce0106e9cbb11d28d5630dac47d8244b6427206
Tuhin <uiu.tuhin@gmail.com>
2021-11-17 10:01:05 +0600

    afte_install script update with if comdition

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -2,9 +2,3 @@
 cd /var/www
 npm install
 npm install --save react react-dom react-scripts react-particles-js
-if which pm2 > /dev/null
-    then
-        echo "node is installed, skipping..."
-    else
-        npm install pm2 -g
-fi
