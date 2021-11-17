cef52d61433389e5d2cb34a4ec48e78d51fcbe4b
Tuhin <uiu.tuhin@gmail.com>
2021-11-17 10:26:47 +0600

    afte_install script update with if comdition

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 cd /var/www
+echo "npm installing..."
 npm install
+echo "npm installed"
+
+echo "react installing..."
 npm install --save react react-dom react-scripts react-particles-js
+echo "npm installed"
+
+echo "pm2 installing..."
+npm install pm2 -g
+echo "pm2 installed"
